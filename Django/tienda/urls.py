160376063495986fce51caa2c70b06dfd8aea27c
Nechaiter <164061016+Nechaiter@users.noreply.github.com>
2024-06-11 14:49:28 -0400

    Delete Django directory

--- Django/tienda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .views import index,contactanos,About,Galeria
-
-urlpatterns = [
-    path('index', index ,name='index'),
-    path('contactanos', contactanos ,name='contactanos'),
-    path('About', About ,name='About'),
-    path('Galeria', Galeria ,name='Galeria'),
-]
