2eb78b119d149e3178a6ea13ff13bed14a53a6e2
Francisco <fr.esteva@duocuc.cl>
2024-06-17 20:34:09 -0400

    login v1.0.2

--- Django/tienda/static/JavaScript/Google.js
+++
@@ -1,8 +0,0 @@
-function AbrirCuenta(){
-    var cuenta = document.getElementById("pop-up");
-    if (cuenta.style.display === "none" || cuenta.style.display === "") {
-        cuenta.style.display = "flex";
-    } else {
-        cuenta.style.display = "none";
-    }
-}
