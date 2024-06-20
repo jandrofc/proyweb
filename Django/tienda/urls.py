0ca10a11ad25b48942448d61a26e998e69af6eeb
Francisco <fr.esteva@duocuc.cl>
2024-06-20 16:27:54 -0400

    diablos

--- Django/tienda/urls.py
+++ Django/tienda/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path 
-from .views import index,Contactanos,Nosotros,Galeria,Login
 from django.contrib.auth.decorators import login_required
+
+from .views import index,Contactanos,Nosotros,Galeria,Login,Registro,Boleta,Carrito
 
 urlpatterns = [
     path('Inicio', index ,name='index'),
     path('Nosotros', Nosotros ,name='Nosotros'),
     path('Galeria', Galeria ,name='Galeria'),
     path('Contactanos', Contactanos ,name='Contactanos'),
+    
+
     path('Login', Login ,name='Login'),
+    path('Registro', Registro ,name='Registro'),
+    path('Boleta', Boleta ,name='Boleta'),
+    path('Carrito', Carrito ,name='Carrito'),
+
+
 ]
--- Django/tienda/views.py
+++ Django/tienda/views.py
@@ -3,21 +3,46 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+#Inicio
 def index (request):
     return render(request,'index.html')
 
+#formulario de contacto
 def Contactanos (request):
     return render(request,'Paginas/contactanos.html')
 
+#Nosotros
 def Nosotros (request):
     return render(request,'Paginas/Quienes_somos.html')
-
+#Galeria de productos
 def Galeria (request):
     return render(request,'Paginas/Galeria.html')
 
+
+#Paginas de cuenta
+
+def Login (request):
+    return render(request,'registration/Login.html')
+
+def Registro (request):
+    return render(request,'registration/Registro.html')
+
+#boleta
+def Boleta (request):
+    return render(request,'Paginas/Boleta.html')
+
+#carrito
+def Carrito (request):
+    return render(request,'Paginas/Carrito.html')
+
+
+
+'''
 @login_required
 def Login (request):
     request.session["usuario"]="admin"
     usuario = request.session["usuario"]
     context = {'usuario':usuario}
     return render(request, 'Login.html', context)
+'''
