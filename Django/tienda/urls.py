71e64b27e815df8f55a14fde7573e0de2bf31661
Nechaiter <fr.esteva@duocuc.cl>
2024-06-20 16:45:45 -0400

    sios

--- Django/tienda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .views import index,Contactanos,Nosotros,Galeria
-
-urlpatterns = [
-    path('Inicio', index ,name='index'),
-    path('Nosotros', Nosotros ,name='Nosotros'),
-    path('Galeria', Galeria ,name='Galeria'),
-    path('Contactanos', Contactanos ,name='Contactanos'),
-]
