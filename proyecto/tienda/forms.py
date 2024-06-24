1d632d2259b52790d8ce78985648a0f59d3c5741
CrisExe <cris.beforus@hotmail.com>
2024-06-23 22:39:15 -0400

    Muchas cosas

--- proyecto/tienda/forms.py
+++ proyecto/tienda/forms.py
@@ -20,6 +20,54 @@ class RegistroUserForm(UserCreationForm):
             raise forms.ValidationError('El correo ya está registrado')
         return email
     
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['rut', 'username', 'nombre', 'apellido', 'correo', 'telefono', 'direccion', 'TipoPago']
+        labels = {
+            'rut': 'Rut',
+            'username': 'Nombre de usuario',
+            'nombre': 'Nombre',
+            'apellido': 'Apellido',
+            'correo': 'Correo',
+            'telefono': 'Teléfono',
+            'direccion': 'Dirección',
+            'TipoPago': 'Tipo de pago',
+        }
+        widgets = {
+            'rut': forms.TextInput(attrs={
+                'placeholder':'Ingrese un rut..',
+                'id': 'rut',
+                'class': 'form-control'}),
+            'username': forms.TextInput(attrs={
+                'placeholder':'Ingrese un nombre de usuario..',
+                'id': 'username',
+                'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={
+                'placeholder':'Ingrese un nombre..',
+                'id': 'nombre',
+                'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={
+                'placeholder':'Ingrese un apellido..',
+                'id': 'apellido',
+                'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={
+                'placeholder':'Ingrese un correo..',
+                'id': 'correo',
+                'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={
+                'placeholder':'Ingrese un teléfono..',
+                'id': 'telefono',
+                'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={
+                'placeholder':'Ingrese una dirección..',
+                'id': 'direccion',
+                'class': 'form-control'}),
+            'TipoPago': forms.Select(attrs={
+                'placeholder':'Ingrese un tipo de pago..',
+                'id': 'tipopago',
+                'class': 'form-control'}),
+        }
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
